6cf21d537ca6b9847c6eb105f49e07a35fccafbc
Beth McKinney <143623621+macbetthh@users.noreply.github.com>
2024-07-16 16:40:59 -0600

    first commit (had to transfer files from old pc - updated my pc) - getting starter code going - got npm installed, db set up, sql user created, and columns made

---
+++ routes/api/category-routes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [Product],
+    });
+    res.json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [Product],
+    });
+    if (!category) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
